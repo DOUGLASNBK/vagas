e68499b0bbb2a81aa1a3997d4e1240f1c85de877
GuilhermeCaruso <gui.caruso@hotmail.com>
2019-07-01 09:44:23 -0300

    adicionado webview

---
+++ build/precache-manifest.80314e9541d1ea29e6582296640975ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9403d03019232a58beff02097338b4d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86a46644780e245a1be4",
+    "url": "/static/js/2.b5c7896e.chunk.js"
+  },
+  {
+    "revision": "ebe6f6a45fc1661f5130",
+    "url": "/static/js/main.1df81f33.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "77885d05131a1f7c5f7d3340ab89bf41",
+    "url": "/static/media/gopher.77885d05.jpeg"
+  }
+]);
